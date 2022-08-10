0bec95b3be62b0448b24e6b92b7ae8c10445c6fa
mani1 <mani1@NANI143>
2022-08-10 15:23:29 +0530

    [Rajeswari]Added UC3

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿namespace FindMaximumString
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            
+
+            MaximumString maximum = new MaximumString();
+            string str = maximum.MaximumStringValue("Apple", "Peach", "Banaana");
+
+            Console.WriteLine("The MaximumString is: " + str);
+        }
+    }
+}
