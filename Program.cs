b82ba480628a1e786fe3e827099da762a32b63c6
mani1 <mani1@NANI143>
2022-08-10 15:14:32 +0530

    finding The Maximum Integer

--- Program.cs
+++ Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace FindMaximumInteger
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("***Welcome To Find Maximum Numbe***");
+            MaximumIntegerNum maximum = new MaximumIntegerNum();
+            int value = maximum.MaximumInteger(55, 77, 84);
+
+            Console.WriteLine("The Maximum Integer is:- " + value);
+        }
+    }
+}
