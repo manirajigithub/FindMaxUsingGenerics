f1a59b0ca994a493f2034b452502930cc8a88833
mani1 <mani1@NANI143>
2022-08-10 15:49:09 +0530

    [Rajeswari]Added Refactor 2

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿namespace ProblemUsingGenerics
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+
+            Calculate<int> greatest = new Calculate<int>();
+            greatest.Comparison(230, 543, 565);
+            Calculate<string> great = new Calculate<string>();
+            great.Comparison("Apple", "Peach", "banana");
+            Calculate<float> greatTwo = new Calculate<float>();
+            greatTwo.Comparison(1.00002F, 2.001F, 1.001F);
+        }
+    }
+
+
+}
