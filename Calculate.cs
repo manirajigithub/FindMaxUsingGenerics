36080be5f012a853d5762ce3fac75cafb140a82f
mani1 <mani1@NANI143>
2022-08-10 16:06:16 +0530

    [Rajeswari]Added Program Using Params in int String float

---
+++ Calculate.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+
+namespace GreatestUsingParams
+
+{
+    public class Calculate<uType> where uType : IComparable
+    {
+        uType[] a;    //variable
+        public Calculate(params uType[] array)   //constructor
+        {
+            this.a = array;
+        }
+        public void sort()
+        {
+            Array.Sort(this.a);
+        }
+        public uType Comparison()
+        {
+            return a[a.Length - 1];
+        }
+        public void Show()
+        {
+            foreach (var uType in a)
+            {
+                Console.WriteLine("value:" + uType);
+            }
+        }
+    }
+}
