aacf34e2087914e15bb06f20bcd09b6a1ee992bf
mani1 <mani1@NANI143>
2022-08-10 15:18:16 +0530

    [Rajeswari]Added UC2

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿namespace FindMaximumNumber
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            
+
+            FindMaximumFloat maximum = new FindMaximumFloat();
+            double value = maximum.MaximumFloatNumber(5.6, 8.9, 4.4);
+
+            Console.WriteLine("The Maximum Float is:- " + value);
+        }
+    }
+}
