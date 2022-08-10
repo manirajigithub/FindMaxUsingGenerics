bfdeba43802f90f59a9883e1f494305ec710d93d
mani1 <mani1@NANI143>
2022-08-10 15:45:37 +0530

    [Rajeswari]Added Program by using string,float,integer

---
+++ Generics.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+
+namespace ProblemUsingGenerics
+{
+    public class Calculate<uType> where uType : IComparable
+    {
+        public void Comparison(uType a, uType b, uType c)
+        {
+            if (a.CompareTo(b) > 0 && a.CompareTo(c) > 0 || a.CompareTo(b) > 0 && a.CompareTo(c) > 0 || a.CompareTo(b) > 0 && a.CompareTo(c) > 0)
+            {
+                Console.WriteLine("Greatest : {0}", a);
+            }
+            else if (b.CompareTo(a) > 0 && b.CompareTo(c) > 0 || b.CompareTo(a) > 0 && b.CompareTo(c) > 0 || b.CompareTo(a) > 0 && b.CompareTo(c) > 0)
+            {
+                Console.WriteLine("Greatest : {0}", b);
+            }
+            else if (c.CompareTo(b) > 0 && c.CompareTo(a) > 0 || c.CompareTo(b) > 0 && c.CompareTo(a) > 0 || c.CompareTo(b) > 0 && c.CompareTo(a) > 0)
+            {
+                Console.WriteLine("Greatest : {0}", c);
+            }
+            else
+            {
+                Console.WriteLine("All three numbers are equal or greatest two are equal");
+            }
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -10,8 +10,11 @@
             Calculate<string> great = new Calculate<string>();
             great.Comparison("Apple", "Peach", "banana");
             Calculate<float> greatTwo = new Calculate<float>();
-        }   greatTwo.Comparison(1.00002F, 2.001F, 1.001F); };
+            greatTwo.Comparison(1.00002F, 2.001F, 1.001F);
+        }
     }
+       
+    
 }
 
 
